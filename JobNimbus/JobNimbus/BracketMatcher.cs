6689f58499671774d74c59acf648ab38eb0f599e
ykhopkar3 <yogendra.khopkar@gmail.com>
2020-08-05 22:45:45 -0600

    first commit

---
+++ JobNimbus/JobNimbus/BracketMatcher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobNimbus
+{
+    /// <summary>
+    /// Class provides functionality to match brackets in a string
+    /// </summary>
+    public class BracketMatcher
+    {
+        /// <summary>
+        /// Verifies if the string input has matching opening and closing brackets
+        /// </summary>
+        /// <param name="input">
+        /// The input string to check
+        /// </param>
+        /// <returns>
+        /// true if the brackets match. false otherwise
+        /// </returns>
+        public static Boolean Match(string input)
+        {
+            Boolean result = false;
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return true;
+            }
+
+            if (input.StartsWith("}"))
+            {
+                return false;
+            }
+
+            Stack<char> stk = new Stack<char>();
+            char[] arrInput = input.ToCharArray();
+            for (int i = 0; i < arrInput.Length; i++)
+            {
+                if (arrInput[i] == '{')
+                {
+                    stk.Push(arrInput[i]);
+                }
+                if (arrInput[i] == '}')
+                {
+                    if (stk.Count > 0)
+                    {
+                        stk.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            //If we reach the end of the string and the stack has items left , means the brackets are not matching
+            result = stk.Count == 0 ? true : false;
+
+            return result;
+        }
+    }
+}
---
+++ JobNimbus/JobNimbus/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace JobNimbus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+}
