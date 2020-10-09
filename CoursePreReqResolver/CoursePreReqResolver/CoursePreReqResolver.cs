54897ef96931586bbf1192ecd850ef61b3a680a5
Yogendra Khopkar <Engineer.YogendraK@gmail.com>
2020-10-09 13:46:47 -0600

    Adding the initial input parser classes

---
+++ CoursePreReqResolver/CoursePreReqResolver/CoursePreReqResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursePreReqResolver
+{
+    /// <summary>
+    /// This class is like a Facade. 
+    /// It specifies the <c = IINputParser></c> and also selects the formatter to format the output. 
+    /// </summary>
+    public class CoursePreReqResolver
+    {
+        public string GetOrderOfCourses(string[] input)
+        {
+            string result = String.Empty;
+
+            try
+            {
+                //Use the parser to parse the input into output
+
+                //call the formatter to appropriately format the output
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CoursePreReqResolver/CoursePreReqResolver/InputParser/ArrayInputParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursePreReqResolver.InputParser
+{
+    public class ArrayInputParser : IInputParser
+    {
+        public IEnumerable<string> ParseInput(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
