1d9a264577f7805fef317481a90d49f45c2f4de9
Yogendra khopkar <Engineer.YogendraK@gmail.com>
2020-10-09 13:32:25 -0600

    Adding the CoursePreReq project

---
+++ CoursePreReqResolver/CoursePreReqResolverTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoursePreReqResolverTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
