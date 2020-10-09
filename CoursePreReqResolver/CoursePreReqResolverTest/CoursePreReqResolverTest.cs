acbd21b67957780ca4f21f817c0bdd76a4f2a46b
Yogendra Khopkar <Engineer.YogendraK@gmail.com>
2020-10-09 13:49:22 -0600

    Adding unit test file.

--- CoursePreReqResolver/CoursePreReqResolverTest/CoursePreReqResolverTest.cs
+++ CoursePreReqResolver/CoursePreReqResolverTest/CoursePreReqResolverTest.cs
@@ -3,11 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CoursePreReqResolverTest
 {
     [TestClass]
-    public class UnitTest1
+    public class CoursePreReqResolverTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEmptyArrayInput()
         {
+            
         }
     }
 }
